5e8ef22bfc895b3177d053288ef9304a794c0e75
Rupi-Jain <rupi.jain@gmail.com>
2021-08-24 14:31:09 -0400

    First commit to typewriter file

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+let counter = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, counter += 50)
+ 
+}
+
+setTimeout(() => console.log('\n') , counter += 100);
+;
